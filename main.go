739563afc5eebb44905b0cb3c6ff878ac90b88cc
Ubuntu <ubuntu@ip-172-31-31-18.us-east-2.compute.internal>
2020-10-25 22:29:05 +0000

    Ping! Pong! Motherfucka

--- main.go
+++ main.go
@@ -2,35 +2,65 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/bwmarrin/discordgo"
 )
 
-const token string = "NzcwMDAyMzExODc4OTM0NTI4.X5XOiQ.Z9F3_0y55l_VScYv7qx_zbV38rg"
+const Token string = "NzcwMDAyMzExODc4OTM0NTI4.X5XOiQ.Z9F3_0y55l_VScYv7qx_zbV38rg"
+
 var BotID string
 
 func main() {
-	dg, err := discordgo.New("Bot " + token)
 
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("error creating Discord session,", err)
 		return
 	}
 
-	u, err := dg.User("@me")
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
 
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
 
-	BotID = u.ID
-
+	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
 
-  if err != nil {
-	  fmt.Println(err.Error())
-    return
-  }
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
 
-	fmt.Println("Bot is running!")
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "g!ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "g!pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
