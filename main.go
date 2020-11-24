d9481aad2426a5e35c1badd86ce1975a2f1fa990
Diginyx <josuextorres@gmail.com>
2020-11-23 17:24:06 -0700

    Finished Music Functionality

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 const Token string = "NzcwMDAyMzExODc4OTM0NTI4.X5XOiQ.Z9F3_0y55l_VScYv7qx_zbV38rg"
 
 var game_running = false
+var music_running = false
 var trivia_game_running = false
 var BotID string
 
@@ -28,13 +29,20 @@ func main() {
 
 	// Register the runProgram func as a callback for MessageCreate events.
 	dg.AddHandler(runProgram)
-	dg.AddHandler(guildCreate)
 
-	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
+	//// Register ready as a callback for the ready events.
+	//dg.AddHandler(ready)
+	//
+	//// Register guildCreate as a callback for the guildCreate events.
+	//dg.AddHandler(guildCreate)
 
-	// Open a websocket connection to Discord and begin listening.
+	// We need information about guilds (which includes their channels),
+	// messages and voice states.
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates)
+
+	// Open the websocket and begin listening.
 	err = dg.Open()
+
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
@@ -65,25 +73,52 @@ func runProgram(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	if m.Content == "m!play" && !music_running {
+		music_running = true
+		s.UpdateStatus(0, "music!");
+		musicplayer.MusicPlayer(s, m, "")
+	}
+
+	if m.Content == "m!stop" {
+		music_running = false
+		musicplayer.MusicPlayer(s, m, "")
+	}
+
+	if m.Content == "m!gag" {
+		music_running = true
+		s.UpdateStatus(0, "gag music!");
+		fmt.Print("gag")
+		musicplayer.MusicPlayer(s, m, "music/gag/")
+	}
+
+	if m.Content == "m!playpop" {
+		music_running = true
+		s.UpdateStatus(0, "pop music!");
+		musicplayer.MusicPlayer(s, m, "music/pop/")
+	}
+
 	// If the message is "pong" reply with "Ping!"
 	if m.Content == "g!pong" {
+		s.UpdateStatus(0, "pong!");
 		s.ChannelMessageSend(testing, "Ping!")
 		return
 	}
 
 	// If the message is "pog" reply with ":gitpog:"
 	if m.Content == "pog" {
+		s.UpdateStatus(0, "pog!");
 		s.ChannelMessageSend(testing, "<:gitpog:770159988915044352>")
 		return
 	}
 
-	if m.Content == "g!hangman restart" {
+	if m.Content == "g!hangman stop" {
 		games.Restart(s, m)
 		game_running = false
 		return
 	}
 
 	if m.Content == "g!hangman" || game_running == true {
+		s.UpdateStatus(0, "hangman!");
 		if !game_running {
 			games.Hangman(s, m, game_running)
 			game_running = true
@@ -94,6 +129,7 @@ func runProgram(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if m.Content == "g!trivia" || trivia_game_running == true {
+		s.UpdateStatus(0, "trivia!");
 		if trivia_game_running {
 			games.Trivia(s, m, trivia_game_running)
 			trivia_game_running = true
@@ -102,49 +138,28 @@ func runProgram(s *discordgo.Session, m *discordgo.MessageCreate) {
 		trivia_game_running = games.Trivia(s, m, trivia_game_running)
 		return
 	}
-
-	if m.Content == "sup" {
-		s.ChannelMessageSend(testing, "sup")
-		return
-	}
-
-	if m.Content == "m!play" {
-		// Find the channel that the message came from.
-		c, err := s.State.Channel(m.ChannelID)
-		if err != nil {
-			// Could not find channel.
-			fmt.Println(err)
-			return
-		}
-
-		// Find the guild for that channel.
-		g, err := s.State.Guild(c.GuildID)
-		if err != nil {
-			// Could not find guild.
-			fmt.Println(err)
-			return
-		}
-
-		// Look for the message sender in that guild's current voice states.
-		for _, vs := range g.VoiceStates {
-			if vs.UserID == m.Author.ID {
-				music.MusicPlayer(s, g.ID, vs.ChannelID)
-				return
-			}
-		}
-	}
 }
 
-func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
-
-	if event.Guild.Unavailable {
-		return
-	}
-
-	for _, channel := range event.Guild.Channels {
-		if channel.ID == event.Guild.ID {
-			_, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! Type !airhorn while in a voice channel to play a sound.")
-			return
-		}
-	}
-}
+//// This function will be called (due to AddHandler above) every time a new
+//// guild is joined.
+//func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+//
+//	if event.Guild.Unavailable {
+//		return
+//	}
+//
+//	for _, channel := range event.Guild.Channels {
+//		if channel.ID == event.Guild.ID {
+//			_, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! Type !airhorn while in a voice channel to play a sound.")
+//			return
+//		}
+//	}
+//}
+//
+////This function will be called (due to AddHandler above) when the bot receives
+//// the "ready" event from Discord.
+//func ready(s *discordgo.Session, event *discordgo.Ready) {
+//
+//	// Set the playing status.
+//	s.UpdateStatus(0, "!TayTay")
+//}
